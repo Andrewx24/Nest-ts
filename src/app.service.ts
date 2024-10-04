59ad96db6756d1513cdfb300738e84e18c949596
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-04 13:22:27 -0400

    fixes

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  // Method to return a hello message
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! This is a NestJS app!';
+  }
+
+  // Method to return a goodbye message
+  getGoodbye(): string {
+    return 'Goodbye World! This is a NestJS app!';
   }
 }
